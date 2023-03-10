20ae3d557b9e16fa3aec51503d2a2d47c09c6229
Nguyen Trung Thien <269.tdd@gmail.com>
2023-03-10 12:29:50 +0700

    create project

---
+++ Shop_dotNet/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Shop_dotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop_dotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        ShopEntities1 db = new ShopEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Shop_dotNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Shop_dotNet.MvcApplication" Language="C#" %>
