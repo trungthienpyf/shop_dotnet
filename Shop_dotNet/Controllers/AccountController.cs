8855ed6b014324098a3ad58296f9737e153e6a89
NguyenAnhDuc2510 <nguyenanhduc2510@gmail.com>
2023-03-24 20:41:19 +0700

    123

--- Shop_dotNet/Controllers/AccountController.cs
+++ Shop_dotNet/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Shop_dotNet.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.EnterpriseServices.CompensatingResourceManager;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,14 +11,92 @@ namespace Shop_dotNet.Controllers
 {
     public class AccountController : Controller
     {
+        private ShopEntities1 _dbContext = new ShopEntities1();
         // GET: Account
         public ActionResult Login()
         {
             return View();
         }
+        [HttpPost]
+ 
+        public ActionResult Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var data = _dbContext.customers.Where(s => s.email.Equals(email) && s.passsword.Equals(password)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    Session["Name"] = data.FirstOrDefault().name;
+                    Session["Email"] = data.FirstOrDefault().email;
+                    Session["idUser"] = data.FirstOrDefault().id;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
         public ActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+       
+        public ActionResult Register(customer _user)
+        {
+
+            if (_user.email == null)
+            {
+                ViewBag.emailError = "May nhap vao cho tao";
+                return View();
+            }
+            if (_user.phone == null)
+            {
+                ViewBag.phoneError = "May nhap vao cho tao";
+                return View();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var check2 = _dbContext.customers.FirstOrDefault(s => s.email == _user.email);
+                var check1 = _dbContext.customers.FirstOrDefault(s => s.phone == _user.phone);
+                if (check1 == null)
+                {
+                    if (check2 == null)
+                    {
+                        customer c = new customer
+                        {
+                            name = _user.name,
+                            email = _user.email,
+                            phone = _user.phone,
+                            passsword = _user.passsword,
+                        };
+
+                        _dbContext.customers.Add(c);
+                        _dbContext.SaveChanges();
+                        return RedirectToAction("Login"); ;
+                    }
+                    else
+                    {
+                        ViewBag.emailDuplicate = "Email trung r thang l";
+                        return View();
+                    }
+                }
+                else
+                {
+                    ViewBag.phoneDuplicate = "Phone trung r thang l";
+                    return View();
+                }
+
+            }
+            return View();
+
+
         }
     }
 }
