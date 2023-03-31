bae71b113c3ce9944ca0644b8d871755f426edc4
PhanTrungTABD30822 <112456061+PhanTrungTABD30822@users.noreply.github.com>
2023-03-31 21:24:53 +0700

    abc

---
+++ Shop_dotNet/Scripts/cartController.js
@@ -0,0 +1,68 @@
+﻿var cart = {
+    init: function () {
+        cart.regEvents();
+    },
+    regEvents: function () {
+        $('#btnContinue').off('click').on('click', function () {
+            window.location.href = "/";
+        });
+        $('#btnPayment').off('click').on('click', function () {
+            window.location.href = "/Shopping/Payment";
+        });
+        $('#btnUpdate').off('click').on('click', function () {
+            var listProduct = $('.txtQuantity');
+            var cartList = [];
+            $.each(listProduct, function (i, item) {
+                cartList.push({
+                    Quantity: $(item).val(),
+                    Product: {
+                        ID: $(item).data('id')
+                    }
+                });
+            });
+
+            $.ajax({
+                url: '/Shopping/Update',
+                data: { cartModel: JSON.stringify(cartList) },
+                dataType: 'json',
+                type: 'POST',
+                success: function (res) {
+                    if (res.status == true) {
+                        window.location.href = "/Shopping";
+                    }
+                }
+            })
+        });
+
+        $('#btnDeleteAll').off('click').on('click', function () {
+
+
+            $.ajax({
+                url: '/Shopping/DeleteAll',
+                dataType: 'json',
+                type: 'POST',
+                success: function (res) {
+                    if (res.status == true) {
+                        window.location.href = "/Shopping";
+                    }
+                }
+            })
+        });
+
+        $('.btn-delete').off('click').on('click', function (e) {
+            e.preventDefault();
+            $.ajax({
+                data: { id: $(this).data('id') },
+                url: '/Shopping/Delete',
+                dataType: 'json',
+                type: 'POST',
+                success: function (res) {
+                    if (res.status == true) {
+                        window.location.href = "/Shoppong";
+                    }
+                }
+            })
+        });
+    }
+}
+cart.init();
